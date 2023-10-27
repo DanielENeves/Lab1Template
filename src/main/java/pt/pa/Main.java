65f615ade4d82a71ac62dedb94c50f7e1acc3bc6
DanielENeves <127295235+DanielENeves@users.noreply.github.com>
2023-10-27 10:41:29 +0100

    Nível 1

--- src/main/java/pt/pa/Main.java
+++ src/main/java/pt/pa/Main.java
@@ -1,5 +1,8 @@
 package pt.pa;
 
+import pt.pa.adts.Position;
+import pt.pa.adts.TreeLinked;
+
 /**
  *
  * @author amfs
@@ -10,7 +13,30 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        TreeLinked<String> myTree = new TreeLinked<>("Ecosystem");
+        Position<String> root = myTree.root();
 
+        //Insert dos Filhos do EcoSystem
+        Position<String> posAnchovy = myTree.insert(root, "Anchovy");
+        Position<String> posTuna = myTree.insert(root, "Tuna");
+        Position<String> posShark = myTree.insert(root, "Shark");
+        Position<String> posEagles = myTree.insert(root, "Eagles");
+
+        //Insert dos filhos do Tuna
+        myTree.insert(posTuna, "Mackerel");
+        Position<String> posBarracuda = myTree.insert(posTuna, "Barracuda");
+        myTree.insert(posBarracuda, "Sardine");
+        //Insert dos filhos do Shark
+        myTree.insert(posShark, "Dolphin");
+
+        //Insert dos filhos do Eagles
+        myTree.insert(posEagles, "Snakes");
+        myTree.insert(posEagles, "Rabbits");
+
+        //Método toString()
+        System.out.println("TREE" + myTree);
+        String showTree = myTree.toString();
+        System.out.println(showTree);
     }
 
 
